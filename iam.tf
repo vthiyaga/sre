9b30b4152ba83be1a6da2b0a525bbc98b30999c2
vthiyaga <50639850+vthiyaga@users.noreply.github.com>
2025-05-22 19:49:19 -0400

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_user" "import_user_iam" {
+  force_destroy        = null
+  name                 = "iam-github-user"
+  path                 = "/"
+  permissions_boundary = null
+  tags                 = {
+    Name = "Created_By_TF"
+  }
+ 
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
